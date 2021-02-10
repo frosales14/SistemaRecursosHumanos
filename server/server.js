60c2250b4fac35c7786dafd6786d5e9bf5d9cc71
Daniel <dhocaballero@gmail.com>
2021-02-09 20:48:01 -0600

    Creacion de la conexion MySQL

--- server/server.js
+++ server/server.js
@@ -8,3 +8,24 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running');
 });
+
+//CONEXION A MYSQL
+
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'database-comex.cxisflzbs7ad.us-east-2.rds.amazonaws.com',
+  user     : '',
+  password : '',
+  database : ''
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  else{
+      console.log('CONEXION EXITOSA');
+  }
+});
+ 
+connection.end();
