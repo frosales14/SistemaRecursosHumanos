f5aef9de9eb427cf6b3b74702590fc992b25979d
Carlos Haddad <carlos.haddad@partnerhero.com>
2021-02-10 19:17:23 -0600

    Agregando variables de entorno para la conexion

--- server/server.js
+++ server/server.js
@@ -1,31 +1,47 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors')
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-});
-
-app.listen(3000, () => {
-    console.log('Server is running');
-});
+dotenv.config();
 
 //CONEXION A MYSQL
 
-const mysql      = require('mysql');
+const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host     : 'database-comex.cxisflzbs7ad.us-east-2.rds.amazonaws.com',
-  user     : '',
-  password : '',
-  database : ''
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.PORT,
 });
  
 connection.connect();
- 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  else{
-      console.log('CONEXION EXITOSA');
+
+connection.connect(err => {
+  if (err) {
+      console.log('Not connected to Database');
+      return err;
+  } else {
+      console.log('Connected to the SQL server');
   }
 });
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send("Hello World");
+});
  
-connection.end();
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   else{
+//       console.log('CONEXION EXITOSA');
+//   }
+// });
+ 
+// connection.end();
+
+app.listen(3000, () => {
+  console.log('Server is running');
+});
