dfae5e45d3212bbf217b1f645c41950e71406a78
Mxcixs <52433371+Mxcixs@users.noreply.github.com>
2021-02-22 01:59:31 -0600

    Vista de plazas de trabajo

---
+++ proyecto-reactjs/src/components/containers/puestos.js
@@ -0,0 +1,255 @@
+import React from "react";
+import { Grid, Typography, Container } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Layout from "../components/layout";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  containerMain: {
+    marginBottom: theme.spacing(10),
+  },
+  papers: {
+    padding: theme.spacing(1),
+    textAlign: "center",
+  },
+  title: {
+    fontSize: "2rem",
+    marginBottom: theme.spacing(2),
+  },
+  nombrePuesto: {
+    padding: theme.spacing(2),
+  },
+  infoPuesto: {
+    fontSize: "15px",
+    lineHeight: "25px",
+    letterSpacing: "0em",
+    /*     textTransform: "uppercase", */
+  },
+  infoVacantes: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(1),
+    padding: theme.spacing(1),
+    fontSize: "1.2rem",
+    fontStyle: "bold",
+  },
+  boton: {
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(1),
+    border: 0,
+  },
+  textBoton: {
+    color: "#FFFFFF",
+  },
+}));
+
+const Puestos = () => {
+  const {
+    containerMain,
+    papers,
+    title,
+    nombrePuesto,
+    infoPuesto,
+    infoVacantes,
+    boton,
+    textBoton,
+  } = useStyles();
+  return (
+    <Layout>
+      <Container>
+        <Grid
+          item
+          container
+          spacing={0}
+          xs={12}
+          md={12}
+          lg={12}
+          justify="center"
+        >
+          <Grid item>
+            <Typography variant={"h4"} className={title}>
+              PLAZAS DISPONIBLES
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container className={containerMain} spacing={4}>
+          <Grid item container spacing={1} xs={12} md={6} lg={3}>
+            <Grid item>
+              <Paper className={papers}>
+                <Typography variant={"h4"} className={nombrePuesto}>
+                  PLAZA 1
+                </Typography>
+                <Typography className={infoPuesto}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus,
+                  sed accumsan erat viverra. Curabitur facilisis ornare
+                  ultrices. Aenean ullamcorper egestas convallis. Phasellus ut
+                  dui lacinia, facilisis risus non, tempor augue. Etiam magna
+                  felis, eleifend ac bibendum a, blandit ut justo. Class aptent
+                  taciti sociosqu ad litora torquent per conubia nostra, per
+                  inceptos himenaeos. Vestibulum sed semper quam, sit amet
+                  consequat urna. Vestibulum vitae velit sit amet nisl
+                  vestibulum efficitur vitae ut enim.
+                </Typography>
+                <Grid item>
+                  <Typography className={infoVacantes} variant={"h6"}>
+                    VACANTES DISPONIBLES
+                  </Typography>
+                  <Typography className={infoVacantes} variant={"h6"}>
+                    FECHA DE CONTRATACIÓN
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Button
+                    className={boton}
+                    variant="contained"
+                    color="primary"
+                    disableElevation
+                    size="large"
+                    fullWidth="true"
+                  >
+                    <Typography className={textBoton} variant={"h6"}>
+                      APLICAR
+                    </Typography>
+                  </Button>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid item container spacing={1} xs={12} md={6} lg={3}>
+            <Grid item>
+              <Paper className={papers}>
+                <Typography variant={"h4"} className={nombrePuesto}>
+                  PLAZA 2
+                </Typography>
+                <Typography className={infoPuesto}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus,
+                  sed accumsan erat viverra. Curabitur facilisis ornare
+                  ultrices. Aenean ullamcorper egestas convallis. Phasellus ut
+                  dui lacinia, facilisis risus non, tempor augue. Etiam magna
+                  felis, eleifend ac bibendum a, blandit ut justo. Class aptent
+                  taciti sociosqu ad litora torquent per conubia nostra, per
+                  inceptos himenaeos. Vestibulum sed semper quam, sit amet
+                  consequat urna. Vestibulum vitae velit sit amet nisl
+                  vestibulum efficitur vitae ut enim.
+                </Typography>
+                <Grid item>
+                  <Typography className={infoVacantes} variant={"h6"}>
+                    VACANTES DISPONIBLES
+                  </Typography>
+                  <Typography className={infoVacantes} variant={"h6"}>
+                    FECHA DE CONTRATACIÓN
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Button
+                    className={boton}
+                    variant="contained"
+                    color="primary"
+                    disableElevation
+                    size="large"
+                    fullWidth="true"
+                  >
+                    <Typography className={textBoton} variant={"h6"}>
+                      APLICAR
+                    </Typography>
+                  </Button>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid item container spacing={1} xs={12} md={6} lg={3}>
+            <Grid item>
+              <Paper className={papers}>
+                <Typography variant={"h4"} className={nombrePuesto}>
+                  PLAZA 3
+                </Typography>
+                <Typography className={infoPuesto}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus,
+                  sed accumsan erat viverra. Curabitur facilisis ornare
+                  ultrices. Aenean ullamcorper egestas convallis. Phasellus ut
+                  dui lacinia, facilisis risus non, tempor augue. Etiam magna
+                  felis, eleifend ac bibendum a, blandit ut justo. Class aptent
+                  taciti sociosqu ad litora torquent per conubia nostra, per
+                  inceptos himenaeos. Vestibulum sed semper quam, sit amet
+                  consequat urna. Vestibulum vitae velit sit amet nisl
+                  vestibulum efficitur vitae ut enim.
+                </Typography>
+                <Grid item>
+                  <Typography className={infoVacantes} variant={"h6"}>
+                    VACANTES DISPONIBLES
+                  </Typography>
+                  <Typography className={infoVacantes} variant={"h6"}>
+                    FECHA DE CONTRATACIÓN
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Button
+                    className={boton}
+                    variant="contained"
+                    color="primary"
+                    disableElevation
+                    size="large"
+                    fullWidth="true"
+                  >
+                    <Typography className={textBoton} variant={"h6"}>
+                      APLICAR
+                    </Typography>
+                  </Button>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid item container spacing={1} xs={12} md={6} lg={3}>
+            <Grid item>
+              <Paper className={papers}>
+                <Typography variant={"h4"} className={nombrePuesto}>
+                  PLAZA 4
+                </Typography>
+                <Typography className={infoPuesto}>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus,
+                  sed accumsan erat viverra. Curabitur facilisis ornare
+                  ultrices. Aenean ullamcorper egestas convallis. Phasellus ut
+                  dui lacinia, facilisis risus non, tempor augue. Etiam magna
+                  felis, eleifend ac bibendum a, blandit ut justo. Class aptent
+                  taciti sociosqu ad litora torquent per conubia nostra, per
+                  inceptos himenaeos. Vestibulum sed semper quam, sit amet
+                  consequat urna. Vestibulum vitae velit sit amet nisl
+                  vestibulum efficitur vitae ut enim.
+                </Typography>
+                <Grid item>
+                  <Typography className={infoVacantes} variant={"h6"}>
+                    VACANTES DISPONIBLES
+                  </Typography>
+                  <Typography className={infoVacantes} variant={"h6"}>
+                    FECHA DE CONTRATACIÓN
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Button
+                    className={boton}
+                    variant="contained"
+                    color="primary"
+                    disableElevation
+                    size="large"
+                    fullWidth="true"
+                  >
+                    <Typography className={textBoton} variant={"h6"}>
+                      APLICAR
+                    </Typography>
+                  </Button>
+                </Grid>
+              </Paper>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Puestos;
--- proyecto-reactjs/src/components/routes/appRoutes.js
+++ proyecto-reactjs/src/components/routes/appRoutes.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { Router, Switch, Redirect, Route } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
-import { WelcomeScreen } from '../containers';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { Router, Switch, Redirect, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+/* import { WelcomeScreen } from '../containers';
+ */
+import { Puestos } from "../containers";
 
 const browserHistory = createBrowserHistory();
 
 const AppRoutes = () => {
-    const loadRoutes = () => {
-        return (
-            <Switch>
-                <Route path="/" component={WelcomeScreen} />
-            </Switch>
-        );
-    };
-
+  const loadRoutes = () => {
     return (
-        <Router history={browserHistory}>
-            <CssBaseline />
-            {loadRoutes()}
-        </Router>
+      <Switch>
+        <Route path="/" component={Puestos} />
+      </Switch>
     );
+  };
+
+  return (
+    <Router history={browserHistory}>
+      <CssBaseline />
+      {loadRoutes()}
+    </Router>
+  );
 };
 
 export default AppRoutes;
