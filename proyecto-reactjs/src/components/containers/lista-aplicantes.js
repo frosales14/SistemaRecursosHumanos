2a03f84aaf89c7f12f3cbad06a2fdb329a241ba8
Mxcixs <52433371+Mxcixs@users.noreply.github.com>
2021-03-01 21:44:13 -0600

    Creación de vista para lista de aplicantes

--- proyecto-reactjs/src/components/containers/lista-aplicantes.js
+++ proyecto-reactjs/src/components/containers/lista-aplicantes.js
@@ -1,18 +1,133 @@
-import React from 'react';
-import { Grid, Container, Typography } from '@material-ui/core';
-import Layout from '../components/layout';
+import React from "react";
+import Layout from "../components/layout";
+import { Container, Grid, Button, Typography } from "@material-ui/core";
+import { DataGrid, GridToolbar } from "@material-ui/data-grid";
+import { makeStyles } from "@material-ui/core/styles";
+import Checkbox from "@material-ui/core/Checkbox";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .headers": {
+      backgroundColor: "rgba(2, 221, 251, 1)",
+    },
+  },
+  containerMain: {
+    marginBottom: theme.spacing(6),
+  },
+  titulo: {
+    fontSize: "2rem",
+  },
+}));
 
 const ListaAplicantes = () => {
-    return (
-        <Layout>
-            <Container fixed maxWidth="xl">
-                <Grid container>
-                    {/* logica lista de aplicantes */}
-                    lista de aplicantes
-                </Grid>
-            </Container>
-        </Layout>
-    );
+  const { root, titulo, containerMain } = useStyles();
+
+  const [unchecked, setChecked] = React.useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  const columns = [
+    { field: "id", headerClassName: "headers", headerName: "ID", width: 70 },
+    {
+      field: "nombre",
+      headerClassName: "headers",
+      headerName: "NOMBRE",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "telefono",
+      headerClassName: "headers",
+      headerName: "TELÉFONO",
+      type: "number",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "puntuacion",
+      headerClassName: "headers",
+      headerName: "PUNTUACIÓN",
+      type: "number",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+    },
+    {
+      field: "visto",
+      headerClassName: "headers",
+      headerName: "VISTO",
+
+      headerAlign: "center",
+      renderCell: (params) => (
+        <Checkbox
+          align="center"
+          unchecked={unchecked}
+          onChange={handleChange}
+        />
+      ),
+    },
+    {
+      field: "cv",
+      headerClassName: "headers",
+      headerName: "CV",
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: (params) => (
+        <Button variant="contained" color="secondary" size="small" fullWidth>
+          VER
+        </Button>
+      ),
+    },
+  ];
+
+  const rows = [
+    { id: 1, nombre: "Juan Perez", telefono: 98144565, puntuacion: 100 },
+    { id: 2, nombre: "Juan Perez", telefono: 98144565, puntuacion: 100 },
+    { id: 3, nombre: "Juan Perez", telefono: 98144565, puntuacion: 100 },
+    { id: 4, nombre: "Juan Perez", telefono: 98144565, puntuacion: 100 },
+    { id: 5, nombre: "Juan Perez", telefono: 98144565, puntuacion: 100 },
+    { id: 6, nombre: "Juan Perez", telefono: 98144565, puntuacion: 100 },
+    { id: 7, nombre: "Juan Perez", telefono: 98144565, puntuacion: 100 },
+    { id: 8, nombre: "Juan Perez", telefono: 98144565, puntuacion: 100 },
+  ];
+  return (
+    <Layout>
+      <Container>
+        <Grid item container justify="center" spacing={4}>
+          <Grid item>
+            <Typography className={titulo}>APLICANTES</Typography>
+          </Grid>
+        </Grid>
+        <Grid
+          container
+          className={containerMain}
+          spacing={10}
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item container md={12} xs={12}>
+            <Grid item md={12} xs={12} sm={12} className={root}>
+              <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={5}
+                autoHeight="true"
+                disableSelectionOnClick="true"
+                components={{
+                  Toolbar: GridToolbar,
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Container>
+    </Layout>
+  );
 };
 
 export default ListaAplicantes;
--- proyecto-reactjs/src/components/containers/puestos.js
+++ proyecto-reactjs/src/components/containers/puestos.js
@@ -1,212 +1,189 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import {
-    Grid,
-    Typography,
-    Container,
-    InputLabel,
-    FormControl,
-    Select,
-    MenuItem,
-} from '@material-ui/core';
-import Button from '@material-ui/core/Button';
-import Layout from '../components/layout';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
+  Grid,
+  Typography,
+  Container,
+  InputLabel,
+  FormControl,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import Layout from "../components/layout";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 
 const NavLink = styled.a`
-    text-decoration: none;
+  text-decoration: none;
 `;
 
 const useStyles = makeStyles((theme) => ({
-    containerMain: {
-        marginBottom: theme.spacing(10),
-    },
-    papers: {
-        padding: theme.spacing(1),
-        textAlign: 'center',
-    },
-    title: {
-        fontSize: '2rem',
-        marginBottom: theme.spacing(2),
-    },
-    nombrePuesto: {
-        padding: theme.spacing(2),
-    },
-    infoPuesto: {
-        fontSize: '15px',
-        lineHeight: '25px',
-        letterSpacing: '0em',
-        /*     textTransform: "uppercase", */
-    },
-    infoVacantes: {
-        marginTop: theme.spacing(2),
-        marginBottom: theme.spacing(1),
-        padding: theme.spacing(1),
-        fontSize: '1.2rem',
-        fontStyle: 'bold',
-    },
-    boton: {
-        marginTop: theme.spacing(2),
-        padding: theme.spacing(1),
-        border: 0,
-    },
-    textBoton: {
-        color: '#FFFFFF',
-    },
-    formControl: {
-        minWidth: 200,
-    },
+  containerMain: {
+    marginBottom: theme.spacing(10),
+  },
+  papers: {
+    padding: theme.spacing(1),
+    textAlign: "center",
+  },
+  title: {
+    fontSize: "2rem",
+    marginBottom: theme.spacing(2),
+  },
+  nombrePuesto: {
+    padding: theme.spacing(2),
+  },
+  infoPuesto: {
+    fontSize: "15px",
+    lineHeight: "25px",
+    letterSpacing: "0em",
+    /*     textTransform: "uppercase", */
+  },
+  infoVacantes: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(1),
+    padding: theme.spacing(1),
+    fontSize: "1.2rem",
+    fontStyle: "bold",
+  },
+  boton: {
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(1),
+    border: 0,
+  },
+  textBoton: {
+    color: "#FFFFFF",
+  },
+  formControl: {
+    minWidth: 200,
+  },
 }));
 
 const Puestos = () => {
-    const {
-        containerMain,
-        papers,
-        title,
-        nombrePuesto,
-        infoPuesto,
-        infoVacantes,
-        boton,
-        textBoton,
-        formControl,
-    } = useStyles();
+  const {
+    containerMain,
+    papers,
+    title,
+    nombrePuesto,
+    infoPuesto,
+    infoVacantes,
+    boton,
+    textBoton,
+    formControl,
+  } = useStyles();
 
-    const valoresDrop = [
-        {
-            valor: 'IT',
-        },
-        {
-            valor: 'Marketing',
-        },
-        {
-            valor: 'Marketing',
-        },
-    ];
+  const valoresDrop = [
+    {
+      valor: "IT",
+    },
+    {
+      valor: "Marketing",
+    },
+    {
+      valor: "Marketing",
+    },
+  ];
 
-    const puestos = [
-        {
-            titulo: 'Plaza 1',
-            descripcion:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus, sed accumsan erat viverra. Curabitur facilisi ornare ultrices. Aenean ullamcorper egestasconvallis. Phasellus ut dui lacinia, facilisis risus non, tempor augue. Etiam magna felis, eleifend ac bibendum a, blandit ut justo. Class aptent taciti sociosqu ad  litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sed semper  quam, sit amet consequat urna. Vestibulum vitae velit sit amet nisl vestibulum efficitur vitae ut enim.',
-            numVacantes: 'VACANTE DISPONIBLES',
-            hireDate: 'FECHA DE CONTRATACION',
-        },
-        {
-            titulo: 'Plaza 2',
-            descripcion:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus, sed accumsan erat viverra. Curabitur facilisi ornare ultrices. Aenean ullamcorper egestasconvallis. Phasellus ut dui lacinia, facilisis risus non, tempor augue. Etiam magna felis, eleifend ac bibendum a, blandit ut justo. Class aptent taciti sociosqu ad  litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sed semper  quam, sit amet consequat urna. Vestibulum vitae velit sit amet nisl vestibulum efficitur vitae ut enim.',
-            numVacantes: 'VACANTE DISPONIBLES',
-            hireDate: 'FECHA DE CONTRATACION',
-        },
-        {
-            titulo: 'Plaza 3',
-            descripcion:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus, sed accumsan erat viverra. Curabitur facilisi ornare ultrices. Aenean ullamcorper egestasconvallis. Phasellus ut dui lacinia, facilisis risus non, tempor augue. Etiam magna felis, eleifend ac bibendum a, blandit ut justo. Class aptent taciti sociosqu ad  litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sed semper  quam, sit amet consequat urna. Vestibulum vitae velit sit amet nisl vestibulum efficitur vitae ut enim.',
-            numVacantes: 'VACANTE DISPONIBLES',
-            hireDate: 'FECHA DE CONTRATACION',
-        },
-        {
-            titulo: 'Plaza 4',
-            descripcion:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus, sed accumsan erat viverra. Curabitur facilisi ornare ultrices. Aenean ullamcorper egestasconvallis. Phasellus ut dui lacinia, facilisis risus non, tempor augue. Etiam magna felis, eleifend ac bibendum a, blandit ut justo. Class aptent taciti sociosqu ad  litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sed semper  quam, sit amet consequat urna. Vestibulum vitae velit sit amet nisl vestibulum efficitur vitae ut enim.',
-            numVacantes: 'VACANTE DISPONIBLES',
-            hireDate: 'FECHA DE CONTRATACION',
-        },
-    ];
+  const puestos = [
+    {
+      titulo: "Plaza 1",
+      descripcion:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus, sed accumsan erat viverra. Curabitur facilisi ornare ultrices. Aenean ullamcorper egestasconvallis. Phasellus ut dui lacinia, facilisis risus non, tempor augue. Etiam magna felis, eleifend ac bibendum a, blandit ut justo. Class aptent taciti sociosqu ad  litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sed semper  quam, sit amet consequat urna. Vestibulum vitae velit sit amet nisl vestibulum efficitur vitae ut enim.",
+      numVacantes: "VACANTE DISPONIBLES",
+      hireDate: "FECHA DE CONTRATACION",
+    },
+    {
+      titulo: "Plaza 2",
+      descripcion:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus, sed accumsan erat viverra. Curabitur facilisi ornare ultrices. Aenean ullamcorper egestasconvallis. Phasellus ut dui lacinia, facilisis risus non, tempor augue. Etiam magna felis, eleifend ac bibendum a, blandit ut justo. Class aptent taciti sociosqu ad  litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sed semper  quam, sit amet consequat urna. Vestibulum vitae velit sit amet nisl vestibulum efficitur vitae ut enim.",
+      numVacantes: "VACANTE DISPONIBLES",
+      hireDate: "FECHA DE CONTRATACION",
+    },
+    {
+      titulo: "Plaza 3",
+      descripcion:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus, sed accumsan erat viverra. Curabitur facilisi ornare ultrices. Aenean ullamcorper egestasconvallis. Phasellus ut dui lacinia, facilisis risus non, tempor augue. Etiam magna felis, eleifend ac bibendum a, blandit ut justo. Class aptent taciti sociosqu ad  litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sed semper  quam, sit amet consequat urna. Vestibulum vitae velit sit amet nisl vestibulum efficitur vitae ut enim.",
+      numVacantes: "VACANTE DISPONIBLES",
+      hireDate: "FECHA DE CONTRATACION",
+    },
+    {
+      titulo: "Plaza 4",
+      descripcion:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ac blandit leo. Sed efficitur nisi vel leo luctus, sed accumsan erat viverra. Curabitur facilisi ornare ultrices. Aenean ullamcorper egestasconvallis. Phasellus ut dui lacinia, facilisis risus non, tempor augue. Etiam magna felis, eleifend ac bibendum a, blandit ut justo. Class aptent taciti sociosqu ad  litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum sed semper  quam, sit amet consequat urna. Vestibulum vitae velit sit amet nisl vestibulum efficitur vitae ut enim.",
+      numVacantes: "VACANTE DISPONIBLES",
+      hireDate: "FECHA DE CONTRATACION",
+    },
+  ];
 
-    return (
-        <Layout>
-            <Container>
-                <Grid item container spacing={0} justify="center">
+  return (
+    <Layout>
+      <Container>
+        <Grid item container spacing={0} justify="center">
+          <Grid item>
+            <Typography variant={"h4"} className={title}>
+              PLAZAS DISPONIBLES
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container item justify="center" spacing={4}>
+          <Grid item>
+            <FormControl className={formControl}>
+              <InputLabel>Departamentos</InputLabel>
+              <Select>
+                {valoresDrop.map((item) => {
+                  return <MenuItem value={item.valor}>{item.valor}</MenuItem>;
+                })}
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+
+        <Grid container className={containerMain} spacing={3}>
+          <Grid item container spacing={3} md={12} xs={12} direction="row">
+            {puestos.map((puesto, index) => {
+              return (
+                <Grid item key={index} md={3} xs={12} sm={6}>
+                  <Paper className={papers}>
+                    <Typography variant={"h4"} className={nombrePuesto}>
+                      {puesto.titulo}
+                    </Typography>
+                    <Typography className={infoPuesto}>
+                      {puesto.descripcion}
+                    </Typography>
                     <Grid item>
-                        <Typography variant={'h4'} className={title}>
-                            PLAZAS DISPONIBLES
-                        </Typography>
+                      <Typography className={infoVacantes} variant={"h6"}>
+                        {puesto.numVacantes}
+                      </Typography>
+                      <Typography className={infoVacantes} variant={"h6"}>
+                        {puesto.hireDate}
+                      </Typography>
                     </Grid>
-                </Grid>
-
-                <Grid container item justify="center" spacing={4}>
                     <Grid item>
-                        <FormControl className={formControl}>
-                            <InputLabel>Departamentos</InputLabel>
-                            <Select>
-                                {valoresDrop.map((item) => {
-                                    return (
-                                        <MenuItem value={item.valor}>
-                                            {item.valor}
-                                        </MenuItem>
-                                    );
-                                })}
-                            </Select>
-                        </FormControl>
+                      <NavLink href="/registro">
+                        <Button
+                          className={boton}
+                          variant="contained"
+                          color="primary"
+                          disableElevation
+                          size="large"
+                          fullWidth="true"
+                        >
+                          <Typography className={textBoton} variant={"h6"}>
+                            APLICAR
+                          </Typography>
+                        </Button>
+                      </NavLink>
                     </Grid>
+                  </Paper>
                 </Grid>
-
-                <Grid container className={containerMain} spacing={3}>
-                    <Grid
-                        item
-                        container
-                        spacing={3}
-                        md={12}
-                        xs={12}
-                        direction="row"
-                    >
-                        {puestos.map((puesto, index) => {
-                            return (
-                                <Grid item key={index} md={3} xs={12} sm={6}>
-                                    <Paper className={papers}>
-                                        <Typography
-                                            variant={'h4'}
-                                            className={nombrePuesto}
-                                        >
-                                            {puesto.titulo}
-                                        </Typography>
-                                        <Typography className={infoPuesto}>
-                                            {puesto.descripcion}
-                                        </Typography>
-                                        <Grid item>
-                                            <Typography
-                                                className={infoVacantes}
-                                                variant={'h6'}
-                                            >
-                                                {puesto.numVacantes}
-                                            </Typography>
-                                            <Typography
-                                                className={infoVacantes}
-                                                variant={'h6'}
-                                            >
-                                                {puesto.hireDate}
-                                            </Typography>
-                                        </Grid>
-                                        <Grid item>
-                                            <NavLink href="/registro">
-                                                <Button
-                                                    className={boton}
-                                                    variant="contained"
-                                                    color="primary"
-                                                    disableElevation
-                                                    size="large"
-                                                    fullWidth="true"
-                                                >
-                                                    <Typography
-                                                        className={textBoton}
-                                                        variant={'h6'}
-                                                    >
-                                                        APLICAR
-                                                    </Typography>
-                                                </Button>
-                                            </NavLink>
-                                        </Grid>
-                                    </Paper>
-                                </Grid>
-                            );
-                        })}
-                    </Grid>
-                </Grid>
-            </Container>
-        </Layout>
-    );
+              );
+            })}
+          </Grid>
+        </Grid>
+      </Container>
+    </Layout>
+  );
 };
 
 export default Puestos;
